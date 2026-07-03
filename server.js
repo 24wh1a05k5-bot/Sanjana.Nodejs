ec8d4fe89819fa4448fc78d3ee3b017708191847
srisanjana00 <sanjanaeerukonda1996@gmail.com>
2026-07-03 21:45:01 +0530

    Fix Render deployment

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static(__dirname));
+app.use("/Frontend", express.static(path.join(__dirname, "Frontend")));
 
 // Home page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
+
+// Autosuggest page
+app.get("/autosuggest", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "autosuggest.html"));
 });
 
 app.listen(PORT, () => {
