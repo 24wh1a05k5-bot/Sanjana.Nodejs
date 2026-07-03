39e6a0dcf220b9f7b0905fb5f2bc9b2d4adef1eb
srisanjana00 <sanjanaeerukonda1996@gmail.com>
2026-07-03 22:11:08 +0530

    Fix Render deployment

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
+// Serve root static files (index.js)
+app.use(express.static(__dirname));
+
+// Serve images
 app.use("/Frontend", express.static(path.join(__dirname, "Frontend")));
+
+// Serve pages
 app.use("/pages", express.static(path.join(__dirname, "pages")));
 
-// Serve root page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "index.html"));
+    res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
